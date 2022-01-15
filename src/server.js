a04cb6dd6a35f9c2cbfca76224126f048e864234
Pavel <pikcelll@yandex-team.ru>
2022-01-15 20:05:02 +0300

    GIX

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ async function refreshCyclically() {
     await updateTreelikeState();
     console.log(`Обновил древовидный стейт по турнирам`);
     console.log(`Начал обновление стейта по уровням`);
-    updateStateByLevel();
+    // updateStateByLevel();
     console.log(`Завершил обновление стейта по уровням`);
     await new Promise((res, rej) => setTimeout(res, 21600000));
   }
